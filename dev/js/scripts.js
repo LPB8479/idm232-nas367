9d33b8307483936122d6c97498a5b46f0b41f78e
Noah Simon <76528559+LPB8479@users.noreply.github.com>
2024-11-27 20:30:13 -0500

    🧑‍💻 tiny changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,9 +2,9 @@ function hamburger() {
     const burgerBtn = document.getElementById("hamburger");
     const header = document.querySelector("header");
     burgerBtn.addEventListener("click", () => {
-        if(header.classList.contains("closed")) {
+        if (header.classList.contains("closed")) {
             header.classList.replace("closed", "open");
-        } else{
+        } else {
             header.classList.replace("open", "closed")
         }
     })
@@ -14,27 +14,29 @@ hamburger();
 function filterMenu() {
     //collapse filter menu
     const filterButton = document.getElementById("filterButton");
-    document.getElementById('filterResults').style.display = "none"
-    filterButton.addEventListener("click", () => {
-        if(!filterButton.classList.contains("active")) {
+    if (filterButton) {
+        document.getElementById('filterResults').style.display = "none"
+        filterButton.addEventListener("click", () => {
+            if (!filterButton.classList.contains("active")) {
                 $('#filterResults').slideDown(500);
                 filterButton.style.backgroundColor = "var(--orange)";
                 filterButton.style.borderColor = 'var(--orange)';
                 filterButton.classList.add("active");
-        } else {
-            $('#filterResults').slideUp(500);
-            filterButton.style.backgroundColor = "var(--yellow)";
-            filterButton.style.borderColor = 'black';
-            filterButton.classList.remove("active");
-        }
-    })
-
-    //filter buttons
-    const filters = document.querySelectorAll(".searchFilter");
-    filters.forEach((filterButton) => {
-        filterButton.addEventListener("click", () => {
-            filterButton.classList.toggle("active");
+            } else {
+                $('#filterResults').slideUp(500);
+                filterButton.style.backgroundColor = "var(--yellow)";
+                filterButton.style.borderColor = 'black';
+                filterButton.classList.remove("active");
+            }
         })
-    })
+
+        //filter buttons
+        const filters = document.querySelectorAll(".searchFilter");
+        filters.forEach((filterButton) => {
+            filterButton.addEventListener("click", () => {
+                filterButton.classList.toggle("active");
+            })
+        })
+    }
 }
 filterMenu();
