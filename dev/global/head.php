5b09827801e5cb7b87a6a4f91e66b4a8cec528f3
Noah Simon <76528559+LPB8479@users.noreply.github.com>
2024-11-27 20:29:03 -0500

    🗃️ Setup db stuff

--- dev/global/head.php
+++ dev/global/head.php
@@ -1,3 +1,15 @@
+<?php
+//uncomment to debug
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+
+    require_once 'includes/db.php';
+    $statement = $connection->prepare('SELECT * FROM recipes');
+    $statement -> execute();
+    $recipes = $statement->get_result()->fetch_all(MYSQLI_ASSOC);
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
