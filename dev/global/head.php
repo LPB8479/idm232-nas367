4471b59da5bc96f9b680bc7b3dca597daf025aaf
Noah Simon <76528559+LPB8479@users.noreply.github.com>
2024-12-12 20:56:51 -0500

    💄 fix font

--- dev/global/head.php
+++ dev/global/head.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/e337a80932.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <title>IDM232-nas367</title>
 </head>
